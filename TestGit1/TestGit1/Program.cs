c23b1cf5fdaf1901ed3f7b5c245a5dbfb0b53d2d
Rajib Saha <rajib.saha@diageo.com>
2015-03-31 07:17:58 -0400

    modified files Test Git

--- TestGit1/TestGit1/Program.cs
+++ TestGit1/TestGit1/Program.cs
@@ -17,6 +17,7 @@ namespace TestGit1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //test after modifying the files
         }
     }
 }
